7c4243c5d6ce8aae0ff2b9c7a509244098048ce0
BaptisteJ29 <44016988+BaptisteJ29@users.noreply.github.com>
2019-03-07 11:56:29 +0100

    Update MatriceInverse.java

--- src/versionFinale/MatriceInverse.java
+++ src/versionFinale/MatriceInverse.java
@@ -1,51 +1,12 @@
 package versionFinale;
 
 public class MatriceInverse implements IMatrice {
-
-//	private Matrice matrice;
-
-//	public MatriceInverse(Matrice matrice) {
-//		super();
-//		this.matrice = matrice;
-//	}
 	
 	private double[][] matriceInverse;
 	
 	public MatriceInverse() {
 		super();
 	}
-
-//	public Matrice getInverse() {
-//		Matrice matriceInverse = this.matrice;
-//		Matrice comatrice = new Matrice(new double[this.matrice.getDimension()][this.matrice.getDimension()]);
-//		double det = this.matrice.getDeterminant();
-//		
-//		if (this.getDimension() == 2) {
-//			double temp = matriceInverse.getValueAt(0, 0);
-//			comatrice.setValueAt(0, 0, this.getValueAt(1, 1));
-//			matriceInverse.setValueAt(0, 0, this.getValueAt(1, 1) * (1 / det));
-//			comatrice.setValueAt(0, 1,  -(this.getValueAt(0, 1)));
-//			matriceInverse.setValueAt(0, 1, -(this.getValueAt(0, 1) * (1 / det)));
-//			comatrice.setValueAt(1, 0,  -(this.getValueAt(1, 0)));
-//			matriceInverse.setValueAt(1, 0, -this.getValueAt(1, 0) * (1 / det));
-//			comatrice.setValueAt(1, 1,  temp);
-//			matriceInverse.setValueAt(1, 1, temp * (1/det));
-//		} else {
-//			matriceInverse = new Matrice(new double[this.getDimension()][this.getDimension()]);
-//			Matrice tmp = null;
-//
-//			for (int i = 0; i < this.getDimension(); i++) {
-//				for (int j = 0; j < this.getDimension(); j++) {
-//					tmp = this.getNewMatrice(i, j);
-//					comatrice.setValueAt(j, i, tmp.getDeterminant() * Math.pow(-1, i + j));
-//					matriceInverse.setValueAt(i, j, Math.pow(-1, i + j) * (tmp.getDeterminant() / det));
-//				}
-//			}
-//			matriceInverse = matriceInverse.getMatriceTranspose();
-//		}
-//		this.afficherMatriceInverse(comatrice, det);
-//		return matriceInverse;
-//	}
 	
 	public double[][] getInverse(Matrice matrice) {
 		this.matriceInverse = matrice.getMatrice();
